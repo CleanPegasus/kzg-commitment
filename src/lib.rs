5ead9fbf5b64a19eb0f884d61a5e8e163502d26f
CleanPegasus <arunk609@gmail.com>
2024-07-10 12:05:26 +0530

    ✅ Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,28 @@
-
 use ark_bls12_381::{Bls12_381, Config, Fr as F, G1Affine, G1Projective, G2Affine, G2Projective};
 use ark_ec::{
     bls12::{G1Prepared, G2Prepared},
     pairing::Pairing,
+    short_weierstrass::Affine,
     AffineRepr, CurveGroup,
 };
 use ark_ff::{Field, UniformRand, Zero};
-use ark_poly::{polynomial, univariate::{DensePolynomial, DenseOrSparsePolynomial}, DenseUVPolynomial, Polynomial,};
+use ark_poly::{
+    polynomial,
+    univariate::{DenseOrSparsePolynomial, DensePolynomial},
+    DenseUVPolynomial, Polynomial,
+};
 use ark_std::{rand, One};
+
+use derive_more::Display;
+
+#[derive(Debug, Display)]
+pub enum ProofError {
+    #[display(fmt = "Cannot generate valid proof: division remainder is non-zero")]
+    InvalidProof,
+    #[display(fmt = "Polynomial division failed")]
+    DivisionError,
+}
+impl std::error::Error for ProofError {}
 
 struct KZGCommitment {
     trusted_setup_g1: Vec<G1Affine>,
@@ -15,7 +30,7 @@ struct KZGCommitment {
 }
 
 impl KZGCommitment {
-    fn new(degree: usize) -> Self {
+    pub fn new(degree: usize) -> Self {
         let (trusted_setup_g1, trusted_setup_g2) = Self::trusted_setup(degree);
         Self {
             trusted_setup_g1,
@@ -25,32 +40,15 @@ impl KZGCommitment {
 
     fn lagrange_interpolation(points: &Vec<(F, F)>) -> DensePolynomial<F> {
         let mut result: DensePolynomial<F> = DensePolynomial::zero();
-
         for (index, &(x_i, y_i)) in points.into_iter().enumerate() {
             let mut term = DensePolynomial::from_coefficients_vec(vec![y_i]);
             for (j, &(x_j, _)) in points.iter().enumerate() {
                 if j != index {
                     let scalar = (x_i - x_j).inverse().unwrap();
-                    let numerator = DensePolynomial::from_coefficients_vec(vec![-x_j * scalar, F::one() * scalar]);
-                    term = &term * &numerator;
-                }
-            }
-
-            result += &term;
-        }
-        result
-    }
-
-    fn lagrange(points: &Vec<(F, F)>) -> DensePolynomial<F> {
-
-        let mut result: DensePolynomial<F> = DensePolynomial::zero();
-
-        for (index, &(x_i, y_i)) in points.into_iter().enumerate() {
-            let mut term = DensePolynomial::from_coefficients_vec(vec![y_i]);
-            for (j, &(x_j, _)) in points.iter().enumerate() {
-                if j != index {
-                    let scalar = (x_i - x_j).inverse().unwrap();
-                    let numerator = DensePolynomial::from_coefficients_vec(vec![-x_j * scalar, F::one() * scalar]);
+                    let numerator = DensePolynomial::from_coefficients_vec(vec![
+                        -x_j * scalar,
+                        F::one() * scalar,
+                    ]);
                     term = &term * &numerator;
                 }
             }
@@ -74,12 +72,10 @@ impl KZGCommitment {
         (trusted_setup_g1, trusted_setup_g2)
     }
 
-    fn vector_to_polynomial(vector: Vec<F>) -> DensePolynomial<F> {
+    pub fn vector_to_polynomial(vector: &Vec<i32>) -> DensePolynomial<F> {
+        let y_s: Vec<F> = vector.iter().map(|&y| F::from(y)).collect();
         let x_s: Vec<F> = (0..vector.len()).map(|val| F::from(val as u32)).collect();
-        dbg!(&x_s);
-        let points: Vec<(F, F)> = x_s.into_iter().zip(vector.into_iter()).collect();
-        dbg!(&points);
-        println!("{:?}", Self::lagrange_interpolation(&points));
+        let points: Vec<(F, F)> = x_s.into_iter().zip(y_s.into_iter()).collect();
         Self::lagrange_interpolation(&points)
     }
 
@@ -103,34 +99,46 @@ impl KZGCommitment {
         result
     }
 
-    fn commit_polynomial(&self, polynomial: &DensePolynomial<F>) -> G1Affine {
+    pub fn commit_polynomial(&self, polynomial: &DensePolynomial<F>) -> G1Affine {
         self.evaluate_polynomial_at_g1_setup(polynomial)
     }
 
-    fn generate_proof(&self, polynomial: &DensePolynomial<F>, points: &Vec<(F, F)>) -> G1Affine {
+    pub fn generate_proof(
+        &self,
+        polynomial: &DensePolynomial<F>,
+        points: &Vec<(F, F)>,
+    ) -> Result<G1Affine, ProofError> {
         // lagrange interpolation
         let point_polynomial = Self::lagrange_interpolation(&points);
         let numerator = polynomial - &point_polynomial;
         let mut denominator = DensePolynomial::from_coefficients_vec(vec![F::from(1)]);
         for (x, _) in points {
-            denominator = &denominator
-                * &DensePolynomial::from_coefficients_vec(vec![-*x, F::from(1)]);
+            denominator =
+                &denominator * &DensePolynomial::from_coefficients_vec(vec![-*x, F::from(1)]);
+        }
+        let (q, r) = DenseOrSparsePolynomial::from(numerator)
+            .divide_with_q_and_r(&DenseOrSparsePolynomial::from(denominator))
+            .unwrap();
+
+        if r != DensePolynomial::zero() {
+            return Err(ProofError::InvalidProof);
         }
 
-        let (q, r) = DenseOrSparsePolynomial::from(numerator).divide_with_q_and_r(&DenseOrSparsePolynomial::from(denominator)).unwrap();
-        assert_eq!(r, DensePolynomial::zero(), "Cannot generate valid proof");
-
-
-        self.evaluate_polynomial_at_g1_setup(&q)
+        Ok(self.evaluate_polynomial_at_g1_setup(&q))
     }
 
-    fn verify_proof(&self, commitment: &G1Affine, points: &Vec<(F, F)>, proof: &G1Affine) -> bool {
+    pub fn verify_proof(
+        &self,
+        commitment: &G1Affine,
+        points: &Vec<(F, F)>,
+        proof: &G1Affine,
+    ) -> bool {
         let point_polynomial = Self::lagrange_interpolation(&points);
 
         let mut vanishing_polynomial = DensePolynomial::from_coefficients_vec(vec![F::from(1)]);
         for (x, _) in points {
             vanishing_polynomial = &vanishing_polynomial
-                * &DensePolynomial::from_coefficients_vec(vec![ -*x, F::from(1)]);
+                * &DensePolynomial::from_coefficients_vec(vec![-*x, F::from(1)]);
         }
 
         let z_s: G2Affine = self.evaluate_polynomial_at_g2_setup(&vanishing_polynomial);
@@ -142,5 +150,84 @@ impl KZGCommitment {
 
         lhs == rhs
     }
+}
 
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use crate::ProofError;
+
+    use super::KZGCommitment;
+    use ark_bls12_381::{Fr as F, G1Affine, G2Affine};
+    use ark_poly::{polynomial, Polynomial};
+    use rand::{prelude::SliceRandom, Rng};
+
+    #[test]
+    fn test_vec_to_poly() {
+        let vector = generate_random_vec();
+        let polynomial = KZGCommitment::vector_to_polynomial(&vector);
+        let random_points = random_points(&vector);
+        for point in random_points {
+            assert_eq!(
+                polynomial.evaluate(&point.0),
+                point.1,
+                "Vector interpolation is wrong"
+            );
+        }
+    }
+
+    #[test]
+    fn test_verify_proof_valid() {
+        let kzg = KZGCommitment::new(50);
+        let vector = generate_random_vec();
+        let polynomial = KZGCommitment::vector_to_polynomial(&vector);
+        let random_points = random_points(&vector);
+        let commitment = kzg.commit_polynomial(&polynomial);
+        let proof = kzg.generate_proof(&polynomial, &random_points).unwrap();
+        let verification = kzg.verify_proof(&commitment, &random_points, &proof);
+
+        assert!(verification, "Verification is false");
+    }
+
+    #[test]
+    fn test_invalid_proof_generation() {
+        let kzg = KZGCommitment::new(50);
+        let vector = generate_random_vec();
+        let polynomial = KZGCommitment::vector_to_polynomial(&vector);
+
+        let invalid_vector = generate_random_vec();
+        let invalid_points = random_points(&invalid_vector);
+
+        let proof_result = kzg.generate_proof(&polynomial, &invalid_points);
+        match proof_result {
+          Ok(_) => panic!("Expected an error, but proof generation succeeded"),
+          Err(ProofError::InvalidProof) => {
+              // Test passes if we get the InvalidProof error
+          },
+          Err(e) => panic!("Expected InvalidProof error, but got: {:?}", e),
+      }
+    }
+
+    fn generate_random_vec() -> Vec<i32> {
+        let mut rng = rand::thread_rng();
+
+        // Generate a random length between 1 and 20
+        let length = rng.gen_range(1..=50);
+        println!("Generating vector with length: {}", length);
+        // Generate the vector with random numbers
+        (0..length).map(|_| rng.gen_range(-1000..=1000)).collect()
+    }
+
+    fn random_points(vec: &Vec<i32>) -> Vec<(F, F)> {
+        let mut rng = rand::thread_rng();
+        let count = rng.gen_range(1..vec.len());
+        println!("Fetching {} points", count);
+        vec.iter()
+            .enumerate()
+            .collect::<Vec<(usize, &i32)>>()
+            .choose_multiple(&mut rng, count)
+            .map(|&(index, item)| (F::from(index as i32), F::from(item.clone())))
+            .collect()
+    }
 }
